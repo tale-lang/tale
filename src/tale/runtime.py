31ae2af480566a17caec0ca719858b105750dd59
JoshuaLight <ashkariel.eter@gmail.com>
2020-03-22 21:42:58 +0200

    Cleaned up comments for `runtime.evaluate`.

--- src/tale/runtime.py
+++ src/tale/runtime.py
@@ -3,4 +3,12 @@ from typing import Any
 from tale.syntax import Node
 
 def evaluate(tree: Node) -> Any:
+    """Evaluates syntax tree node and returns a result.
+
+    This method is used by the `core` module after successful parsing of syntax tree.
+
+    Args:
+        tree: A root node of syntax tree that represents the program.
+    """
+
     return ''
