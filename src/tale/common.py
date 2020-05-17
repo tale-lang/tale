c49792bcbd2e498220fecffc9521bc491cb3301d
JoshuaLight <j.light.developer@gmail.com>
2020-05-17 21:00:32 +0300

    Added comments for `pipe` function.

--- src/tale/common.py
+++ src/tale/common.py
@@ -3,6 +3,24 @@ from functools import reduce
 
 
 def pipe(*funcs: List[Callable]) -> Any:
+    """Composes a sequence of functions into single pipe.
+
+    For example, a following code:
+
+        pipe(f, g)(x)
+
+    Is similar to:
+
+        g(f(x))
+
+    Args:
+        funcs: A sequence of functions. 
+
+    Returns:
+        A function that pipes an input value into the first function of the sequence,
+        whose result is piped into the second function, etc.
+    """
+
     def pipe_(f: Callable, g: Callable):
         return lambda x: g(f(x))
 
