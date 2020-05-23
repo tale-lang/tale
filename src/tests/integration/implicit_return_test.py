8968be0ee3051e951054ba12bce846afd45447ad
JoshuaLight <j.light.developer@gmail.com>
2020-05-23 16:11:10 +0300

    Added simple integration test.

---
+++ src/tests/integration/implicit_return_test.py
@@ -0,0 +1,14 @@
+from tale.core import execute
+
+
+def test_one_expression():
+    # Arrange.
+    program = """
+x
+"""
+
+    # Act.
+    out = execute(program)
+
+    # Assert.
+    assert out == 'x'
