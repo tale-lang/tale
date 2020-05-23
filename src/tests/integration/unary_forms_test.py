2105c730b9fe6e6b770878a3e1b9056d78a85dc9
JoshuaLight <j.light.developer@gmail.com>
2020-05-23 20:11:28 +0300

    Added nested unary expressions test.

--- src/tests/integration/unary_forms_test.py
+++ src/tests/integration/unary_forms_test.py
@@ -13,3 +13,17 @@ def test_simple_assignment():
 
     # Assert.
     assert out == '1'
+
+
+def test_nested_expression():
+    # Arrange.
+    program = """
+(x) just = x
+(1 just) just
+"""
+
+    # Act.
+    out = execute(program)
+
+    # Assert.
+    assert out == '1'
