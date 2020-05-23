a4752b75d55c24381d3c7c0d8394188a5a7b1663
JoshuaLight <j.light.developer@gmail.com>
2020-05-23 20:10:02 +0300

    Implemented unary expressions.

--- src/tale/runtime/evaluation.py
+++ src/tale/runtime/evaluation.py
@@ -1,6 +1,20 @@
-from typing import Any, Optional
+from typing import Any, Iterable, Optional
 
-from tale.syntax.nodes import Assignment, Expression, Form, Node, Statement
+from tale.syntax.nodes import (Assignment, Expression, Form, Node,
+                               PrimitiveExpression, PrimitiveForm, Statement,
+                               UnaryExpression, UnaryForm)
+
+
+class CapturedArgument:
+    def __init__(self, name: str, value: Node):
+        self.name = name
+        self.value = value
+
+
+class CapturedExpression:
+    def __init__(self, node: Node, arguments: Iterable[CapturedArgument] = None):
+        self.node = node
+        self.arguments = arguments or []
 
 
 class Binding:
@@ -10,17 +24,35 @@ class Binding:
         self.form = form
         self.value = value
 
-    def captures(self, expression: str) -> bool:
-        """Checks whether the form of the binding could capture an expression.
+    def capture(self, node: Node) -> CapturedExpression:
+        """Captures an expression if it's possible.
 
         Args:
-            expression: A string that represents a piece of code.
+            node: A node that represents an expression.
 
         Returns:
-            `True` if `expression` could be catured by `form`, otherwise `False`.
+            An instance of `CapturedExpression` if `expression` matches the
+            form of the binding, otherwise `None`.
         """
 
-        return self.form.content == expression
+        def captures_simple(form: PrimitiveForm, node: PrimitiveExpression):
+            if form.content == node.content:
+                return CapturedExpression(self.value)
+
+        def captures_unary(form: UnaryForm, node: UnaryExpression):
+            if form.identifier == node.identifier:
+                return CapturedExpression(
+                        self.value,
+                        [CapturedArgument(form.argument.name, node.argument)])
+
+        form = self.form
+
+        if isinstance(form, PrimitiveForm) and \
+           isinstance(node, PrimitiveExpression):
+            return captures_simple(form, node)
+        if isinstance(form, UnaryForm) and \
+           isinstance(node, UnaryExpression):
+            return captures_unary(form, node)
 
 
 class Scope:
@@ -31,24 +63,24 @@ class Scope:
             For example, a function scope is a parent for if statement scope.
     """
 
-
     def __init__(self, parent: Optional['Scope'] = None):
         self.parent = parent
         self.bindings = []
 
-    def binding(self, expression: str):
-        """Finds a binding that could capture an expression.
+    def capture(self, node: Node) -> CapturedExpression:
+        """Finds a binding that could capture an expression, and captures it.
 
         Args:
-            expression: An expression to capture.
+            node: A node that represents an expression.
 
         Returns:
-            An instance of `Binding`.
+            An instance of `CapturedExpression`.
         """
 
-        for binding in self.bindings:
-            if binding.captures(expression):
-                return binding
+        captures = (x.capture(node) for x in self.bindings)
+        captures = (x for x in captures if x)
+
+        return next(captures, None)
 
     def bind(self, form: Node, value: Node):
         """Binds the specified value node to the specified form.
@@ -75,15 +107,22 @@ class Scope:
         """
 
         def resolve_assignment(x: Assignment):
+            print('Resolving: ' + x.content)
             self.bind(x.form, x.value)
 
         def resolve_expression(x: Expression):
-            binding = self.binding(x.content)
+            print('Resolving: ' + x.content)
+            captured = self.capture(x)
 
-            if binding is None:
+            if captured is None:
                 return x.content
             else:
-                return resolve_expression(binding.value)
+                scope = Scope(parent=self)
+
+                for arg in captured.arguments:
+                    scope.bind(PrimitiveForm(arg.name), arg.value)
+
+                return scope.resolve(captured.node)
 
         def resolve_statement(x: Statement):
             x = x.children[0]
@@ -93,11 +132,14 @@ class Scope:
             if isinstance(x, Expression):
                 return resolve_expression(x)
 
+        if isinstance(node, Expression):
+            return resolve_expression(node)
+
         result = None
 
-        for child in node.children:
-            if isinstance(child, Statement):
-                result = resolve_statement(child)
+        for x in node.children:
+            if isinstance(x, Statement):
+                result = resolve_statement(x)
 
         return result
 
--- src/tale/syntax/nodes.py
+++ src/tale/syntax/nodes.py
@@ -12,33 +12,35 @@ class Node:
         children: A sequence of child nodes.
     """
 
-    def __init__(self, kind: str, content: str, children: Optional[Sequence['Node']] = None):
-        self.kind = kind
+    def __init__(self, content: str, children: Optional[Sequence['Node']] = None):
         self.content = content
         self.children = children
 
     def __str__(self):
         return format_tree(self,
-                           format_node=lambda x: f'{x.kind} "{x.content}"',
+                           format_node=lambda x: f'{type(x).__name__} "{x.content}"',
                            get_children=lambda x: x.children)
 
 
 class Statement(Node):
-    """A node that represents either an assignment or an expression."""
+    """A statement node.
+
+    Statement is either an expression or an assignment.
+    """
 
 
 class Assignment(Statement):
-    """A node that represents an assignment.
+    """An assignment node.
 
-    For example, the following is the example of an assignment:
+    The following is an example of the assignment:
         y = x
     where:
-        y is a form (the `form` property of the node);
-        x is a value (the `value` property of the node).
+        `y` is a form (the `form` property of the node);
+        `x` is a value (the `value` property of the node).
     """
 
-    def __init__(self, kind: str, content: str, children = None):
-        super().__init__(kind, content, children)
+    def __init__(self, content: str, children = None):
+        super().__init__(content, children)
 
     @property
     def form(self) -> 'Form':
@@ -50,7 +52,40 @@ class Assignment(Statement):
 
 
 class Expression(Statement):
-    ...
+    """An expression node.
+
+    Represents a value that could be captured by form.
+
+    For example, the following is an expression:
+        1 squared
+    It could be captured by the form:
+        (x) squared
+    """
+
+
+class PrimitiveExpression(Expression):
+    """A primitive expression.
+
+    Primitive expression is just a plain value.
+    """
+
+
+class UnaryExpression(Expression):
+    """An unary expression.
+
+    An unary expression consist of two parts: an argument and the identifier.
+    """
+
+    def __init__(self, content: str, children = None):
+        super().__init__(content, children)
+
+    @property
+    def argument(self) -> 'Node':
+        return self.children[0]
+
+    @property
+    def identifier(self) -> str:
+        return self.children[1].content
 
 
 class Form(Node):
@@ -64,3 +99,52 @@ class Form(Node):
     Attributes:
         node: A syntax node that represents the form.
     """
+
+
+class PrimitiveForm(Form):
+    """A primitive form.
+
+    Primitive form captures only plain values.
+    """
+
+
+class UnaryForm(Form):
+    """An unary form.
+
+    An unary form consists of an argument and an identifier.
+
+    For example, a following is an unary form:
+        (x) squared
+    where:
+        `(x)` is a variable argument of a form;
+        `squared` is a simple identifier.
+    """
+
+    def __init__(self, content: str, children = None):
+        super().__init__(content, children)
+
+    @property
+    def argument(self) -> 'Argument':
+        return self.children[0]
+
+    @property
+    def identifier(self) -> str:
+        return self.children[1].content
+
+
+class Argument(Node):
+    """An argument node.
+
+    Arguments are variable parts of forms.
+
+    For example, in the following form:
+        (x) squared
+    `(x)` is an argument with name `x`.
+    """
+
+    def __init__(self, content: str, children = None):
+        super().__init__(content, children)
+
+    @property
+    def name(self) -> str:
+        return self.children[1].content
