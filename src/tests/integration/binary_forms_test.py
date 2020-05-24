9ec56bfb49bf49edfe64a5249dea95c4de530dd3
JoshuaLight <j.light.developer@gmail.com>
2020-05-24 17:30:37 +0300

    Added some tests for binary forms.

--- src/tests/integration/binary_forms_test.py
+++ src/tests/integration/binary_forms_test.py
@@ -14,3 +14,18 @@ def test_first_argument():
 
     # Assert.
     assert out == '1'
+
+
+def test_second_argument():
+    # Arrange.
+    program = """
+(x) + (y) = y
+
+1 + 2
+"""
+
+    # Act.
+    out = execute(program)
+
+    # Assert.
+    assert out == '2'
