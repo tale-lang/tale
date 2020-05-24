0f31337ac2c510c5ee99aafd90ee796bb3167762
JoshuaLight <j.light.developer@gmail.com>
2020-05-24 18:06:37 +0300

    Added invalid comments tests.

--- src/tests/integration/comments_test.py
+++ src/tests/integration/comments_test.py
@@ -1,10 +1,12 @@
+import pytest
+
 from tale.core import execute
 
 
 def test_one_comment():
     # Arrange.
     program = """
--- Test.
+-- A.
 1
 """
 
@@ -13,3 +15,45 @@ def test_one_comment():
 
     # Assert.
     assert out == '1'
+
+
+def test_two_comments():
+    # Arrange.
+    program = """
+-- A.
+-- B.
+1
+"""
+
+    # Act.
+    out = execute(program)
+
+    # Assert.
+    assert out == '1'
+
+
+def test_comments_after_code():
+    # Arrange.
+    program = """
+-- A.
+1
+-- B.
+"""
+
+    # Act.
+    out = execute(program)
+
+    # Assert.
+    assert out == '1'
+
+
+def test_invalid_comment():
+    # Arrange.
+    program = """
+- A.
+1
+"""
+
+    # Act & Assert.
+    with pytest.raises(Exception):
+        out = execute(program)
