f755d40a6da5e554e67900ded29eff0bdb94c456
JoshuaLight <j.light.developer@gmail.com>
2020-05-27 00:49:30 +0300

    Added some tests to `interop_test`.

--- src/tests/integration/interop_test.py
+++ src/tests/integration/interop_test.py
@@ -13,3 +13,31 @@ x
 
     # Assert.
     assert out == 1
+
+
+def test_invalid_first_arg():
+    # Arrange.
+    program = """
+x = py: 1
+x
+"""
+
+    # Act.
+    out = execute(program)
+
+    # Assert.
+    assert out == 'py:1'
+
+
+def test_overriden():
+    # Arrange.
+    program = """
+py: (x) = x
+py: 1
+"""
+
+    # Act.
+    out = execute(program)
+
+    # Assert.
+    assert out == 1
