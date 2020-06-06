f4ce7cdfdb8df8fa99ad1d15bd7c6f45ee86503e
JoshuaLight <j.light.developer@gmail.com>
2020-06-06 21:11:39 +0300

    Added one more test for prefix operators.

--- src/tests/integration/forms/prefix_operators_test.py
+++ src/tests/integration/forms/prefix_operators_test.py
@@ -14,3 +14,18 @@ def test_simple_prefix_operator():
 
     # Assert.
     assert out == 1
+
+
+def test_multiple_prefix_operators_in_expression():
+    # Arrange.
+    program = """
+-(x) = x + 1
+
+-(-1)
+"""
+
+    # Act.
+    out = execute(program)
+
+    # Assert.
+    assert out == 3
