8fb2f9a85ed27d4ea92bbe83abc04291a67e3ed9
JoshuaLight <j.light.developer@gmail.com>
2020-06-06 21:05:40 +0300

    Cleaned up tests.

--- src/tests/integration/forms/prefix_operators_test.py
+++ src/tests/integration/forms/prefix_operators_test.py
@@ -14,18 +14,3 @@ def test_simple_prefix_operator():
 
     # Assert.
     assert out == 1
-
-
-def test_simple_prefix_operator():
-    # Arrange.
-    program = """
--(x) = x
-
--1
-"""
-
-    # Act.
-    out = execute(program)
-
-    # Assert.
-    assert out == 1
