3c42a2fa0ff5d09de7fe0cb28941a0452ee7a8e2
JoshuaLight <j.light.developer@gmail.com>
2020-06-08 23:20:48 +0300

    Cleaned up `vm_test.py`.

--- src/tests/integration/vm/vm_test.py
+++ src/tests/integration/vm/vm_test.py
@@ -5,6 +5,20 @@ def test_simple_literal():
     # Arrange.
     vm = Vm()
     instructions = [
+        PushInt(1),
+    ]
+
+    # Act.
+    vm.execute(instructions)
+
+    # Assert.
+    assert vm.stack == [1]
+
+
+def test_simple_assignment():
+    # Arrange.
+    vm = Vm()
+    instructions = [
         StartBind('x'),
             PushInt(1),
         EndBind(),
